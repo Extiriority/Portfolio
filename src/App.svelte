060da8674c2d15215f09a3772b43add6343cd2f4
Giang <463189@student.fontys.nl>
2022-07-24 17:48:55 +0200

    Add responsive navBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-   import { onMount } from "svelte";
-　　import Navbar from "./lib/Navbar.svelte";
-　　import Intro from "./lib/Intro.svelte";
-   import About from "./lib/About.svelte";
-   import Experience from "./lib/Experience.svelte";
-   import Work from "./lib/Work.svelte";
-   import Contact from "./lib/Contact.svelte";
-   import Footer from "./lib/Footer.svelte";
-
+   import { onMount } from "svelte"
+   import { menu } from "./lib/state/stores";
+　　import Navbar from "./lib/Navbar.svelte"
+　　import Intro from "./lib/Intro.svelte"
+   import About from "./lib/About.svelte"
+   import Experience from "./lib/Experience.svelte"
+   import Work from "./lib/Work.svelte"
+   import Contact from "./lib/Contact.svelte"
+   import Footer from "./lib/Footer.svelte"
 
    let isPageLoaded = false;
 
@@ -21,11 +21,12 @@
        isPageLoaded = false
        window.scrollTo(0,0);
    };
+
    /*https://svelte.dev/repl/a461eb2587e64d73b6328f230ea1b3a1?version=3.49.0*/
 </script>
 
 
-<main class="bg-bkg selection:bg-accent selection:text-bkg ">
+<main class="bg-bkg selection:bg-accent selection:text-bkg">
     {#if !isPageLoaded}
         <div class="min-h-screen flex flex-col justify-evenly items-center relative">
             <img src="/src/assets/Loader.svg" alt="spinner">
