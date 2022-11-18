3dac83705f5bcd20baa74543a3ab7a3c572b3252
Giang <463189@student.fontys.nl>
2022-11-18 21:28:00 +0100

    Change resume button

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -24,8 +24,8 @@
         <img in:fly="{{delay: 50, y: -20, duration: 400 }}" src={logo} alt="Logo" width="60" height="60">
         <input type="checkbox" id="hamburger" class="peer" on:click={toggleNavBar} hidden>
         <label for="hamburger" class="fixed top-0 bottom-0 right-0 w-28 pt-10 w-full peer-checked:hamburger z-50 md:hidden">
-            <div aria-hidden="true" class="m-auto h-0.5 w-6 rounded bg-touch transition duration-300 {show ? '-rotate-45 translate-y-1.5' : null }"></div>
-            <div aria-hidden="true" class="m-auto mt-2 h-0.5 w-6 rounded bg-touch transition duration-300 {show ? 'rotate-45 -translate-y-1' : null }"></div>
+            <span aria-hidden="true" class="m-auto h-0.5 w-6 rounded bg-touch transition duration-300 {show ? '-rotate-45 translate-y-1.5' : null }"></span>
+            <span aria-hidden="true" class="m-auto mt-2 h-0.5 w-6 rounded bg-touch transition duration-300 {show ? 'rotate-45 -translate-y-1' : null }"></span>
         </label>
         <nav class="fixed flex justify-center items-center top-0 bottom-0 z-30 bg-ease p-8 w-full phone:w-9/12 text-2xl md:shadow-none ease-in-out duration-300 md:justify-end md:bg-bkg md:text-base md:wd-auto md:static {show ? 'right-0 shadow-xl shadow-ease' : '-right-full'}">
             <ol class="absolute flex items-center flex-col space-y-12 md:flex-row md:space-x-10 md:space-y-0">
@@ -44,7 +44,7 @@
                 {/each}
                 <li>
                     <button in:fly="{{delay: 550, y: -20, duration: 400 }}"
-                            class="text-touch border rounded-md border-touch px-4 -my-8 py-1 hover:bg-ease hover:py-3 active:scale-90 ease-in-out duration-200"
+                            class="text-touch rounded-md border-touch px-4 -my-8 py-1 hover:bg-ease hover:border-b hover:border-t hover:py-2 active:scale-90 ease-in-out duration-200"
                             onclick="window.open('/src/assets/Resume.pdf','blank')">
                         Resume
                     </button>
