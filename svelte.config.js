bcf85873d25518f29b861989c159ea7fe96ad013
Giang <463189@student.fontys.nl>
2023-07-02 20:38:00 +0200

    add: fix dist folder for cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-import adapter from '@sveltejs/adapter-cloudflare'
+import cloudflare from '@sveltejs/adapter-cloudflare'
 
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess:
       vitePreprocess({
       }),
     kit: {
-      adapter: adapter({}),
+      adapter: cloudflare({}),
     }
 }
