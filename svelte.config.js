cd5283673bc7be4bab0c6566a2197e939852774a
Giang <463189@student.fontys.nl>
2023-07-02 18:17:27 +0200

    add: update npm packages and fix cloudpage

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import sveltePreprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
 import adapter from '@sveltejs/adapter-cloudflare'
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess:
-      sveltePreprocess({
-        postcss: true,
+      vitePreprocess({
       }),
     kit: {
       adapter: adapter({}),
