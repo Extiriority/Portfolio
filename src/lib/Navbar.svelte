405877a644e2e7ec1f94c76f3e9981b112489b16
Giang <463189@student.fontys.nl>
2023-08-10 17:41:33 +0200

    feat: sticky navbar desktop

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-import { fly } from 'svelte/transition'
+    import { fly } from 'svelte/transition'
     import { menu } from './state/stores'
     import logo from '../assets/logo.png'
+    import popout from '../assets/popout.svg'
     import resume from '../assets/resume.png'
 
     let links = [
@@ -17,14 +18,24 @@ import { fly } from 'svelte/transition'
         show = !show
         menu.set(show)
     }
+
+    let prevPos = window.scrollY;
+
+    window.onscroll = function() {
+        const currentPos = window.scrollY;
+        document.getElementById("navbar").style.top = prevPos > currentPos ? "0" : "-85px"
+
+        prevPos = currentPos;
+    }
 </script>
 
-<div class="bg-bkg text-md text-muted pt-1">
+<div class="md:fixed md:w-full z-50 bg-bkg text-md text-muted duration-200 pt-1" id="navbar">
     <div class="flex justify-between mx-auto items-center pl-7 md:pl-10 pr-52 md:pr-7 pt-4">
         <img in:fly="{{delay: 50, y: -20, duration: 400 }}" src={logo} alt="Logo" width="60" height="60">
-        <label for="hamburger" class="fixed top-0 bottom-0 right-0 pt-10 w-28 peer-checked:hamburger z-50 md:hidden">
-            <span aria-hidden="true" class="m-auto h-0.5 w-6 rounded bg-touch transition duration-300 {show ? '-rotate-45 translate-y-1.5' : null }"></span>
-            <span aria-hidden="true" class="m-auto mt-2 h-0.5 w-6 rounded bg-touch transition duration-300 {show ? 'rotate-45 -translate-y-1' : null }"></span>
+        <input type="checkbox" id="hamburger" class="peer" on:click={toggleNavBar} hidden>
+        <label for="hamburger" class="fixed top-0 bottom-0 right-0 pt-10 w-28 h-20 peer-checked:hamburger z-50 md:hidden">
+            <div aria-hidden="true" class="m-auto h-0.5 w-6 rounded bg-touch transition duration-300 {show ? '-rotate-45 translate-y-1.5' : null }"></div>
+            <div aria-hidden="true" class="m-auto mt-2 h-0.5 w-6 rounded bg-touch transition duration-300 {show ? 'rotate-45 -translate-y-1' : null }"></div>
         </label>
         <nav class="fixed flex justify-center items-center top-0 bottom-0 z-30 bg-ease p-8 w-full phone:w-9/12 text-2xl md:shadow-none ease-in-out duration-300 md:justify-end md:bg-bkg md:text-base md:wd-auto md:static {show ? 'right-0 shadow-xl shadow-ease' : '-right-full'}">
             <ol class="absolute flex items-center flex-col space-y-12 md:flex-row md:space-x-10 md:space-y-0">
@@ -32,20 +43,23 @@ import { fly } from 'svelte/transition'
                     <li>
                         <a in:fly="{{delay: link.delayTime, y: -20, duration: 400 }}"
                            on:click={toggleNavBar}
-                           class="flex hover:text-touch duration-200 hover:pt-0.5 active:pt-0"
+                           class="flex hover:text-touch duration-200 border-touch hover:border-b ease-in-out "
                            href="#{link.text}">
-                            <div class="md:flex text-center">
+                            <div class="md:flex text-center py-1">
                                 <span class="text-touch md:pr-1.5">{link.Id}.</span>
                                 <div>{link.text}</div>
                             </div>
                         </a>
                     </li>
                 {/each}
-                <li>
+                <li class="pr-6">
                     <a in:fly="{{delay: 550, y: -20, duration: 400 }}"
-                            class="text-touch rounded-md border-touch px-4 -my-8 py-1 hover:bg-ease hover:border-b hover:border-t hover:py-2 active:scale-90 ease-in-out duration-200"
+                            class="flex hover:text-touch py-1 rounded-md border-touch hover:border-b hover:border-t ease-in-out duration-200"
                             href="{resume}" target="_blank">
-                        Resume
+                        <div class="flex text-center pl-1 py-1">
+                            <img src={popout} alt="popout icon" class="pr-2 center">
+                            <div>Resume</div>
+                        </div>
                     </a>
                 </li>
             </ol>
